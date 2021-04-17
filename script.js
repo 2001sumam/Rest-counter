6797535c4287f473212fc4130d0790c9857aa27f
Suman <2001sumankoley@gmail.com>
2021-04-17 20:11:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// select DOM
+const search = document.querySelector('.search');
+const countrycard = document.querySelector('.country_card');
+const button = document.querySelector('.btn');
+const buttonClear = document.querySelector('.btnc');
+const funk= document.querySelector(".funk")
+// create click event for clear search box
+buttonClear.addEventListener('click',function(){
+    search.value="";
+});
+// create click event
+button.addEventListener('click', function () {
+    const rendercountry = function (data) {
+        const html = `
+                <article class="country">
+                    <img class ="country_img" src="${data.flag}"/>
+                    <div class="right">
+                        <h1>${data.name}</h1>
+                        <h3>${data.region}</h3>
+                        <div class ="lang"><span>🗣</span>${data.languages.map(staklang)}</div>
+                        <h4><span>👨‍👩‍👧‍👦</span>${data.population}</h4>
+                        <h4><span>🕓</span>${data.timezones[0]}</h4>
+                    </div>
+
+                </article>   
+              `
+               return html;
+       console.log(funk);        
+    };
+    // creat language function
+    const staklang = function(flan){
+        const lhtml=`<h4>${flan.name}</h4>`
+        return lhtml;
+    }
+    // fatch API
+    const getcountry = function (country) {
+        fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(function(response){
+            console.log(response);
+            return response.json()
+        }).then(function(sresponse){
+            console.log(sresponse);
+            const res=sresponse;
+           countrycard.innerHTML=res.map(rendercountry);
+        })
+    
+        
+    
+    }
+    // call main function
+    getcountry(search.value);
+    // search.value="";
+});
+
+
